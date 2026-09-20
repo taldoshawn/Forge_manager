eadc6a461823395795ea9b84e6411be86a4eace9
Shawn <156717620+taldoshawn@users.noreply.github.com>
2026-09-20 01:16:41 -0300

    Use provided neon file icons by file type

--- app/src/main/java/com/forgemanager/app/features/explorer/FileListAdapter.kt
+++ app/src/main/java/com/forgemanager/app/features/explorer/FileListAdapter.kt
@@ -45,11 +45,12 @@ class FileListAdapter(
 
         val item = getItem(position)
         val kind = FileTypeClassifier.classify(item.name, item.isDirectory)
+        val icon = iconFor(kind, item.name)
         holder.name.text = item.name
-        holder.icon.setImageResource(iconFor(kind))
-        holder.icon.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(context, colorFor(kind)))
+        holder.icon.setImageResource(icon.resource)
+        holder.icon.imageTintList = if (icon.rich) null else ColorStateList.valueOf(ContextCompat.getColor(context, colorFor(kind)))
         holder.icon.contentDescription = FileTypeClassifier.shortLabel(kind, item.name)
-        holder.icon.alpha = if (item.isDirectory) 1f else 0.94f
+        holder.icon.alpha = 1f
 
         val date = if (item.modified > 0) {
             DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT).format(Date(item.modified))
@@ -64,36 +65,41 @@ class FileListAdapter(
         return view
     }
 
-    private fun iconFor(kind: FileKind): Int = when (kind) {
-        FileKind.DIRECTORY -> R.drawable.ic_file_folder
-        FileKind.APK -> R.drawable.ic_file_apk
-        FileKind.ARCHIVE -> R.drawable.ic_file_archive
-        FileKind.IMAGE -> R.drawable.ic_file_image
-        FileKind.VIDEO -> R.drawable.ic_file_video
-        FileKind.AUDIO -> R.drawable.ic_file_audio
-        FileKind.CODE -> R.drawable.ic_file_code
-        FileKind.SCRIPT -> R.drawable.ic_file_script
-        FileKind.WEB -> R.drawable.ic_file_web
-        FileKind.MARKDOWN -> R.drawable.ic_file_markdown
-        FileKind.TEXT -> R.drawable.ic_file_text
-        FileKind.DOCUMENT -> R.drawable.ic_file_document
-        FileKind.PDF -> R.drawable.ic_file_pdf
-        FileKind.DEX -> R.drawable.ic_file_dex
-        FileKind.XML -> R.drawable.ic_file_xml
-        FileKind.DATABASE -> R.drawable.ic_file_database
-        FileKind.FONT -> R.drawable.ic_file_font
-        FileKind.SPREADSHEET -> R.drawable.ic_file_sheet
-        FileKind.PRESENTATION -> R.drawable.ic_file_presentation
-        FileKind.CONFIG -> R.drawable.ic_file_config
-        FileKind.CERTIFICATE -> R.drawable.ic_file_certificate
-        FileKind.EXECUTABLE -> R.drawable.ic_file_executable
-        FileKind.GENERIC -> R.drawable.ic_file_generic
+    private data class IconSpec(val resource: Int, val rich: Boolean)
+
+    private fun iconFor(kind: FileKind, name: String): IconSpec {
+        val ext = FileTypeClassifier.extensionOf(name)
+        return when (kind) {
+            FileKind.DIRECTORY -> IconSpec(R.drawable.ic_file_folder_rich, true)
+            FileKind.ARCHIVE -> if (ext in setOf("rar", "7z")) IconSpec(R.drawable.ic_file_rar_rich, true)
+                else IconSpec(R.drawable.ic_file_zip_rich, true)
+            FileKind.IMAGE -> if (ext == "svg") IconSpec(R.drawable.ic_file_vector_rich, true)
+                else IconSpec(R.drawable.ic_file_image_rich, true)
+            FileKind.VIDEO -> IconSpec(R.drawable.ic_file_video_rich, true)
+            FileKind.AUDIO -> IconSpec(R.drawable.ic_file_audio_rich, true)
+            FileKind.PDF -> IconSpec(R.drawable.ic_file_pdf_rich, true)
+            FileKind.CODE, FileKind.SCRIPT, FileKind.WEB, FileKind.MARKDOWN -> IconSpec(R.drawable.ic_file_code_rich, true)
+            FileKind.TEXT, FileKind.DOCUMENT -> IconSpec(R.drawable.ic_file_text_rich, true)
+            FileKind.DISK_IMAGE -> IconSpec(R.drawable.ic_file_disk_image, false)
+            FileKind.APK -> IconSpec(R.drawable.ic_file_apk, false)
+            FileKind.DEX -> IconSpec(R.drawable.ic_file_dex, false)
+            FileKind.XML -> IconSpec(R.drawable.ic_file_xml, false)
+            FileKind.DATABASE -> IconSpec(R.drawable.ic_file_database, false)
+            FileKind.FONT -> IconSpec(R.drawable.ic_file_font, false)
+            FileKind.SPREADSHEET -> IconSpec(R.drawable.ic_file_sheet, false)
+            FileKind.PRESENTATION -> IconSpec(R.drawable.ic_file_presentation, false)
+            FileKind.CONFIG -> IconSpec(R.drawable.ic_file_config, false)
+            FileKind.CERTIFICATE -> IconSpec(R.drawable.ic_file_certificate, false)
+            FileKind.EXECUTABLE -> IconSpec(R.drawable.ic_file_executable, false)
+            FileKind.GENERIC -> IconSpec(R.drawable.ic_file_generic, false)
+        }
     }
 
     private fun colorFor(kind: FileKind): Int = when (kind) {
         FileKind.DIRECTORY -> R.color.fm_folder
         FileKind.APK -> R.color.fm_apk
         FileKind.ARCHIVE -> R.color.fm_archive
+        FileKind.DISK_IMAGE -> R.color.fm_disk_image
         FileKind.IMAGE -> R.color.fm_image
         FileKind.VIDEO -> R.color.fm_video
         FileKind.AUDIO -> R.color.fm_audio
