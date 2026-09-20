d860f433ca3cf2f85676fb7678d6241b54cb24bf
Shawn <156717620+taldoshawn@users.noreply.github.com>
2026-09-20 01:31:14 -0300

    Test IMG and disk image classification

--- app/src/test/java/com/forgemanager/app/features/explorer/FileTypeClassifierTest.kt
+++ app/src/test/java/com/forgemanager/app/features/explorer/FileTypeClassifierTest.kt
@@ -1,6 +1,7 @@
 package com.forgemanager.app.features.explorer
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class FileTypeClassifierTest {
@@ -9,9 +10,16 @@ class FileTypeClassifierTest {
         assertEquals(FileKind.APK, FileTypeClassifier.classify("app.apk", false))
         assertEquals(FileKind.ARCHIVE, FileTypeClassifier.classify("backup.7z", false))
         assertEquals(FileKind.IMAGE, FileTypeClassifier.classify("photo.webp", false))
+        assertEquals(FileKind.IMAGE, FileTypeClassifier.classify("cover.img", false))
         assertEquals(FileKind.CODE, FileTypeClassifier.classify("MainActivity.kt", false))
         assertEquals(FileKind.DEX, FileTypeClassifier.classify("classes.dex", false))
         assertEquals(FileKind.DATABASE, FileTypeClassifier.classify("app.sqlite3", false))
+    }
+
+    @Test fun recognizesDiskImageNamesForFallbackInspector() {
+        assertTrue(FileTypeClassifier.isDiskImageName("system.img"))
+        assertTrue(FileTypeClassifier.isDiskImageName("linux.iso"))
+        assertTrue(FileTypeClassifier.isDiskImageName("disk.qcow2"))
     }
 
     @Test fun hiddenExtensionlessFilesStayGeneric() {
